ba3515405498aea6b9375d9a8faf3481e9e39a78
Patrick Cunningham <cunnip06@mc-s083370.local>
2014-01-26 21:59:48 +0000

    Initial Commit

---
+++ assignment/PAC123_AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PAC123_AppDelegate.h
+//  assignment
+//
+//  Created by Patrick Cunningham on 26/01/2014.
+//  Copyright (c) 2014 Patrick Cunningham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PAC123_AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ assignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  assignment
+//
+//  Created by Patrick Cunningham on 26/01/2014.
+//  Copyright (c) 2014 Patrick Cunningham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PAC123_AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PAC123_AppDelegate class]));
+    }
+}
